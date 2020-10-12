f6687e06c88a0190a58b7fc2f2c0e3ce057e8b4c
Melody <mcmelodychen@gmail.com>
2020-10-12 17:00:15 -0400

    fixed showHint, check answer

--- client/components/levelOne.js
+++ client/components/levelOne.js
@@ -1,20 +1,65 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchQuestion} from '../store/questionStore'
+import Modal from 'react-modal'
 
 /**
  * COMPONENT
  */
 
 // Current Q: {this.props.question.question.prompt}
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
+Modal.setAppElement(document.getElementById('form'))
 
 class LevelOne extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      showHint: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.hintOnClick = this.hintOnClick.bind(this)
+  }
+
   componentDidMount() {
     this.props.fetchQuestion()
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    // alert('!!')
+    if (this.state.value == this.props.question.question.plotAnswer) {
+      alert(this.props.question.question.successText)
+    } else {
+      alert('❗Access Prohibited❗')
+    }
+    this.setState({value: ''})
+  }
+
+  hintOnClick(e) {
+    e.preventDefault()
+    this.setState({showHint: !this.state.showHint})
+  }
+
   render() {
-    console.log(this.props)
+    console.log('this.state', this.state)
+    console.log('plotanswer: ', this.props.question.question.plotAnswer)
     return (
       <div className="level-container">
         <div className="flex-child-left">
@@ -25,7 +70,26 @@ class LevelOne extends React.Component {
             <p>Enter SQL queries here.</p>
           </div>
           <div id="textbox">
-            <p>{this.props.question.question.plotQuestion}</p>
+            <form id="form">
+              <label>
+                {this.props.question.question.plotQuestion}
+                <br />
+                <input
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <input type="submit" onClick={this.handleSubmit} />
+            </form>
+            <form>
+              <button onClick={this.hintOnClick}>Hint</button>
+              <div>
+                {this.state.showHint ? (
+                  <div>{this.props.question.question.hint}</div>
+                ) : null}
+              </div>
+            </form>
           </div>
         </div>
 
--- client/questions/level1Q.js
+++ client/questions/level1Q.js
@@ -2,10 +2,10 @@ const level1Q = [
   {
     prompt:
       'Special Agent Q. Your first mission is to crack the database to find the answer.',
-    plotQuestion: 'How many rows does our suspect table have?',
+    plotQuestion: 'How many rows are in the suspect table?',
     plotAnswer: '250',
     hint: 'hint img url',
-    successText: 'Yes!'
+    successText: 'Good job, Q! Now you are in the database.'
   },
   {
     prompt: 'Now, we need to figure out how many suspects in total',
