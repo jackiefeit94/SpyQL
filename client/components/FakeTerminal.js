c56e110d30ac0c5bac59d76de064a2e81df5a08b
Sydney Scott <sydney.roxana@gmail.com>
2020-10-16 16:13:26 -0400

    Update: added more hint functionality

--- client/components/FakeTerminal.js
+++ client/components/FakeTerminal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {CodeEditor} from './CodeEditor'
 import Typed from 'react-typed'
-import KeyboardEventHandler from 'react-keyboard-event-handler'
+import Popup from 'react-animated-popup'
 
 class FakeTerminal extends React.Component {
   constructor() {
@@ -12,7 +12,8 @@ class FakeTerminal extends React.Component {
         'I knew I could count on you. Here’s the deal: there’s been a breach. Someone has stolen a document containing the data of millions of civilians. If we don’t find the source and stop it, people’s personal information could be compromised…',
       questionIdx: 0,
       answer: '',
-      clue: ''
+      clue: '',
+      visible: false
     }
     this.handleQuery = this.handleQuery.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -78,9 +79,20 @@ class FakeTerminal extends React.Component {
                 typedRef={typed => {
                   this.typed = typed
                 }}
-                typeSpeed={40}
+                typeSpeed={35}
               />
             )}
+            <br />
+            {this.props.allQs.length &&
+              this.state.displayMessage.includes('count') && (
+                <Typed
+                  strings={[this.props.allQs[0].prompt]}
+                  startDelay={14000}
+                  typeSpeed={35}
+                  showCursor={false}
+                />
+              )}
+
             {this.state.clue.length > 0 && (
               <button
                 type="submit"
@@ -105,6 +117,26 @@ class FakeTerminal extends React.Component {
             createTable={this.props.createTable}
             handleQuery={this.handleQuery}
           />
+
+          <button
+            type="submit"
+            onClick={() => this.setState({visible: !this.state.visible})}
+          >
+            Teach me
+          </button>
+          {this.props.allQs.length && (
+            <Popup
+              visible={this.state.visible}
+              onClose={() => this.setState({visible: false})}
+            >
+              <p>
+                <img
+                  id="hint"
+                  src={this.props.allQs[this.state.questionIdx].hint}
+                />
+              </p>
+            </Popup>
+          )}
         </div>
         <form id="form" onSubmit={this.handleSubmit}>
           <label>
