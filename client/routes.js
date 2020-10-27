e22c51f393f42640a0f6e7a8f4c80e3fbd975fa3
Melody <mcmelodychen@gmail.com>
2020-10-27 17:39:56 -0400

    no return

--- client/routes.js
+++ client/routes.js
@@ -15,6 +15,39 @@ import {
  * COMPONENT
  */
 class Routes extends Component {
+  constructor(props) {
+    super(props)
+
+    this.currentPathname = null
+    this.currentSearch = null
+  }
+
+  componentDidMount() {
+    const {history} = this.props
+
+    history.listen((newLocation, action) => {
+      if (action === 'PUSH') {
+        if (
+          newLocation.pathname !== this.currentPathname ||
+          newLocation.search !== this.currentSearch
+        ) {
+          // Save new location
+          this.currentPathname = newLocation.pathname
+          this.currentSearch = newLocation.search
+
+          // Clone location object and push it to history
+          history.push({
+            pathname: newLocation.pathname,
+            search: newLocation.search
+          })
+        }
+      } else {
+        // Send user back if they try to navigate back
+        history.go(1)
+      }
+    })
+  }
+
   render() {
     return (
       <Switch>
